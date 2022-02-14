caecc3f3b4c4fa5cfe1b1ae93dabd794c0101fd8
bbpardo <b.b.pardo@protonmail.com>
2022-02-14 14:28:58 +0100

    logos

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function imprimeHola() {
     console.log("hola")
 }
 
+//function prueba(){
+  //  document.getElementById("dates2").style.display = "block";
+  //  console.log("details");
+//}
 
 
 
@@ -10,21 +14,24 @@ function main() {
     const title = document.querySelector("h1");
     title.addEventListener("pointerenter",imprimeHola);
     const elements = Array.from(document.body.querySelectorAll("*"));
-    
-    const counters = elements.map (
-        (item)=> {
-            const obj = {
-                element: item,
-                counter: 0,
-            }
-            item.addEventListener("pointerenter", ()=>{
-                obj.counter++;
-                console.log(obj)
-            });
-            return obj //No es necesario pues obj ya guarda informacion y no necesita un return
-        }
-    );
-    console.log(counters) 
+   // const details = document.querySelector(".dates1")
+   // details.addEventListener("click", prueba);
+
+
+    //const counters = elements.map (
+        //(item)=> {
+            //const obj = {
+               // element: item,
+              //  counter: 0,
+          //  }
+           // item.addEventListener("pointerenter", ()=>{
+               // obj.counter++;
+              //  console.log(obj)
+          //  });
+           // return obj //No es necesario pues obj ya guarda informacion y no necesita un return
+       // }
+  //  );
+    // console.log(counters) 
     
 }
 window.addEventListener("load",main);
